3bbfe69cd624a0623b700485b1c95d39511b19be
Lukas <77298041+lu-make@users.noreply.github.com>
2023-03-04 00:52:06 +0100

    Add files via upload

---
+++ compare.pl
@@ -0,0 +1,38 @@
+use strict;
+use diagnostics;
+
+sub compare($$){
+	my $score = 0;
+	my ($u, $v) = @_;
+	my $ul = length($u);
+	my $vl = length($v);
+	my $tl;
+	$tl = $ul if ($ul >= $vl);
+	$tl = $vl if ($vl > $ul);
+	if($u eq $v){
+		return 100;
+	}
+	
+	my $s;
+	my $t;
+	# todo Sortieren nach Buchstaben
+	for my $i (0 .. ($tl - 1)) {
+		print ("\n" . $i . ", ");
+		$s = substr $u, $i, 1 or $s = "";
+		$t = substr $v, $i, 1 or $t = "";
+		print ("s is undefined! ") if (!$s);
+		print ("t is undefined! ") if (!$t);
+		print ("\"" . $s . "\" == \"" . $t . "\"") if ($s eq $t);
+		print ("\"" . $s . "\" =/= \"" . $t . "\"") unless ($s eq $t);
+		$score++ if ($s eq $t);
+	}
+	print "\n" . $score . " / " . $tl;
+}
+
+print "String 1: ";
+our $input1 = <STDIN>;
+chomp $input1;
+print "String 2: ";
+our $input2 = <STDIN>;
+chomp $input2; 
+compare($input1, $input2);
